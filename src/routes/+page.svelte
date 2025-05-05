94207917c25215903db63e0414d73e182603d4da
noahwessels-mech <114631609+noahwessels-mech@users.noreply.github.com>
2025-05-04 18:36:50 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 <div class="py-4 flex justify-center items-center space-x-8">
     <img src={lathe} alt="MENG shop" class="p-2 w-1/3 flex justify-center"/>
     <div class="w-96">
-        <p class="">3rd year mechanical engineering student at the University of Victoria and the UVic Environmental Engineering Club Project Manager.</p>
+        <p class="">4th year mechanical engineering student at the University of Victoria and the UVic Environmental Engineering Club Project Manager.</p>
         <p class="">In my free time I enjoy playing the banjo, rock climbing, and making violins.</p>
     </div>
 </div>   
